54548a5ecebf4c6f256e5cfeaf4322b502b680a4
Mary-Orlova <vampusha07@gmail.com>
2025-04-20 20:42:47 +0300

    изменение файла router -импорт

--- src/router.py
+++ src/router.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from .database import get_db
-import schemas, models
+from . import schemas, models
 
 
 router = APIRouter(
