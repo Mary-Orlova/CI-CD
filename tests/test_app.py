4d93f77c22893e954d86aa81b56b20a9fe8ac1a4
Mary-Orlova <vampusha07@gmail.com>
2025-04-21 00:36:56 +0300

    редактирование файла тест

--- tests/test_app.py
+++ tests/test_app.py
@@ -115,6 +115,7 @@ async def test_update_recipe(client):
         "cook_time": 60,
         "ingredients": [{"title": "Ингредиент", "quantity": "100г"}]
     }
+
     create_resp = await client.post("/recipes/", json=recipe_data)
     assert create_resp.status_code == 200
     recipe_id = create_resp.json()["id"]
@@ -125,7 +126,6 @@ async def test_update_recipe(client):
         "cook_time": 20,
         "ingredients": [{"title": "Новый ингредиент", "quantity": "200г"}]
     }
-    # Исправленный URL:
     response = await client.patch(f"/recipes/{recipe_id}", json=update_data)
     assert response.status_code == 200, f"Expected 200, but got {response.status_code}.  Response text: {response.text}"
     data = response.json()
